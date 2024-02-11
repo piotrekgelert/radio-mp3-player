189d88268cbea69c79fcd7d23de668ed7af53137
PiotrekGelert <piotrekgelert@gmail.com>
2024-02-11 18:33:24 +0000

    found tags for mp3 player

--- App_main/player.py
+++ App_main/player.py
@@ -1,5 +1,59 @@
-import mutagen
+import sys
+
 import pygame
 import PyQt6.QtCore as qtc
 import PyQt6.QtGui as qtg
 import PyQt6.QtWidgets as qtw
+# from mutagen.mp3 import MP3
+from tinytag import TinyTag as tag
+from UI.player_ui_ui import Ui_mw_main
+
+
+class MainClass(qtw.QMainWindow, Ui_mw_main):
+    song_path = ''
+    folder_path = ''
+
+    songs = {}
+    
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pb_add_file.clicked.connect(self.song_info)
+    
+    def song_info(self):
+        s_path = qtw.QFileDialog.getOpenFileName()
+        file = s_path[0]  # .split('/')[-1]
+        print(file)
+        # print(tag.is_supported(file))
+        info = tag.get(file)
+        # print(f'album: {info.album}')
+        # print(f'album artist: {info.albumartist}')
+        # print(f'artist: {info.artist}')
+        # print(f'composer: {info.composer}')
+        # print(f'title: {info.title}')
+        # print(f'comment: {info.comment}')
+        # print(f'duration: {info.duration}')
+        # print(f'genre: {info.genre}')
+        song = '{} - {} ({})    {}'.format(
+            info.artist,
+            info.title,
+            info.album,
+            info.duration
+        )
+        print(song)
+
+        print(info.bitrate)
+        
+
+        # audio = MP3(file)
+        # print(audio.info.pprint(), '\n\n')
+        # print(mutagen.File(file))
+
+
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    window = MainClass()
+    window.show()
+    sys.exit(app.exec())
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # radio-mp3-player
-The mp3 player with radiio application made in Python 3.12  with help of gamesalad, mutagen and pyqt6
+The mp3 player with radiio application made in Python 3.12  with help of pygame, tinytag and pyqt6
+tinytag-1.10.1
+pygame 2.5.2
